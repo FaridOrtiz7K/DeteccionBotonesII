5c8fde7eb5e6454656fceb465e562a7bc72929c9
Metzli <143217861+m3tzly@users.noreply.github.com>
2025-10-21 12:34:30 -0600

    update v.py

--- versionFinal/v.py
+++ versionFinal/v.py
@@ -5,7 +5,17 @@ import pyautogui
 import pandas as pd
 import cv2
 import numpy as np
+print("Versiones de las librerías utilizadas:")
+print("-------------------------------")
+print('pyautogui')
 print(pyautogui.__version__ )
+print('numpy')
 print(np.__version__ )
+print('pandas')
 print(pd.__version__ )
+print('opencv')
 print(cv2.__version__ )
+print('python')
+print(sys.version )
+print('sistema operativo')
+print(os.name )
