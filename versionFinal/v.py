313866da16e523af37bb1a2c1717f8ac422f9a26
Metzli <143217861+m3tzly@users.noreply.github.com>
2025-10-21 12:29:20 -0600

    verificador de versiones

---
+++ versionFinal/v.py
@@ -0,0 +1,11 @@
+import os
+import time
+import sys
+import pyautogui
+import pandas as pd
+import cv2
+import numpy as np
+print(pyautogui.__version__ )
+print(np.__version__ )
+print(pd.__version__ )
+print(cv2.__version__ )
