8285d72998d5b7e63325d5ece2638d804a55ad25
FaridOrtiz7K <farolinfeo135@gmail.com>
2026-02-04 22:45:09 -0600

    Create datos_globales.py

---
+++ MVC_Final/models/datos_globales.py
@@ -0,0 +1,11 @@
+import threading
+
+class DatosGlobales:
+    CSV_FILE = ""
+    KML_FILENAME = "NN"
+    LINEA_ACTUAL = 0
+    LINEA_MAXIMA = 0
+    LOTE_INICIO = 1
+    LOTE_FIN = 1
+    INFO_LOTE_ACTUAL = {}
+    
